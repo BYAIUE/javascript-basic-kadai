c5a4bea749f951071c33a7ce2c0212c21baabbe3
BYAIUE <166904616+BYAIUE@users.noreply.github.com>
2024-05-11 18:26:05 +0900

    課題_mdn_jsを再提出

--- kadai_mdn_js/mdn.js
+++ kadai_mdn_js/mdn.js
@@ -1,7 +1,18 @@
-let today = new Date();
 
-let year = today.getFullYear();
-let month = today.getMonth() +1;
-let day = today.getDate();
+// let today = new Date();
 
-console.log(year +'年' + month + '月' + day + '日' );
+// let year = today.getFullYear();
+// let month = today.getMonth() +1;
+// let day = today.getDate();
+
+// console.log(year +'年' + month + '月' + day + '日' );
+
+const date = new Date();
+
+const [month, day, year] = [
+    date.getMonth(),
+    date.getDate(),
+    date.getFullYear(),
+];
+
+console.log(year + '年' + month + '月' + day + '日' );
